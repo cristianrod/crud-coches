9fd1a7066da38b20f1f8e1e762d37e07fb9b6552
Cristian R <crisrodruiz@gmail.com>
2018-07-12 13:33:52 +0200

    Agrega plantilla y menu principal

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <title>@yield('titulo')</title>
+</head>
+<body>
+    <div class="container">
+        @yield('contenido')
+    </div>
+</body>
+</html>
---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,14 @@
+@extends ('layouts.master')
+
+@section ('titulo', 'Menú Principal')
+
+@section ('contenido')
+    <div class="row">
+        <div class="col-md-12 text-center mt-4">
+            <h1>Menú principal</h1>
+            <a role="button" href="pantalla2" class="btn btn-primary">Ir a la pantalla 2</a>
+            <a role="button" href="pantalla3" class="btn btn-primary">Ir a la pantalla 3</a>
+        </div>
+    </div>
+@endsection
+
